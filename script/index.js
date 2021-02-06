5a01634bcf33f7fc0370b928bec03bbed8dc2f0c
algnot <thanawat.k2000@gmail.com>
2021-02-06 19:14:59 +0700

    new commit

--- script/index.js
+++ script/index.js
@@ -43,7 +43,7 @@ function getCookie(cname) {
 }
 
 function load(id,path){
-  path = 'http://tongog.xyz/'+path;
+  path = 'https://raw.githubusercontent.com/algnot/web-tech/main/'+path;
   $(function(){
       $(id).load(path)
   })
